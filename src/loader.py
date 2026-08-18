db8a57de4a679545ff0d3f1ca6a8e309bb3872e1
GabrielGustavoDeSouza <gabriel.souza.gustavo@gmail.com>
2026-08-18 11:20:34 -0300

    Add loader.py to load Excel sheets using pandas

---
+++ src/loader.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+ARQUIVO = "data/Controle_Indicadores_Delga_2026.xlsx"
+
+def carregar_abas():
+    return pd.read_excel(
+        ARQUIVO,
+        sheet_name=None,
+        engine="openpyxl"
+    )
