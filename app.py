d8a2adc57c40b085e52ea5737e40624be230bb3f
GabrielGustavoDeSouza <gabriel.souza.gustavo@gmail.com>
2026-08-18 13:34:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,15 @@ import streamlit as st
 import pandas as pd
 
 st.set_page_config(
-    page_title="Dashboard Delga Copilot",
+    page_title="Dashboard Executivo Delga",
+    page_icon="📊",
     layout="wide"
 )
 
-st.title("📊 Dashboard Delga Copilot")
+st.title("📊 Dashboard Executivo Delga")
 
 arquivo = st.file_uploader(
-    "Selecione uma planilha Excel",
+    "Selecione a planilha",
     type=["xlsx"]
 )
 
@@ -20,27 +21,54 @@ if arquivo:
     st.success("Planilha carregada com sucesso")
 
     aba = st.selectbox(
-        "Escolha uma aba",
+        "Selecione uma aba",
         excel.sheet_names
     )
 
     df = pd.read_excel(
-        excel,
-        sheet_name=aba
+        arquivo,
+        sheet_name=aba,
+        header=None
     )
 
-    st.subheader("Prévia dos Dados")
+    st.subheader("Dados Brutos")
 
     st.dataframe(df)
 
-    st.subheader("Resumo")
+    try:
 
-    c1, c2, c3 = st.columns(3)
+        meta = "R$ 50,32 Mi"
+        realizado = "R$ 0"
+        atingimento = "0%"
+        gap = "R$ 50,32 Mi"
 
-    c1.metric("Linhas", len(df))
-    c2.metric("Colunas", len(df.columns))
-    c3.metric("Aba", aba)
+        c1,c2,c3,c4 = st.columns(4)
+
+        c1.metric(
+            "Meta Anual",
+            meta
+        )
+
+        c2.metric(
+            "Realizado",
+            realizado
+        )
+
+        c3.metric(
+            "% Atingimento",
+            atingimento
+        )
+
+        c4.metric(
+            "Gap",
+            gap
+        )
+
+    except:
+        pass
 
 else:
 
-    st.info("Envie um arquivo Excel para iniciar a análise.")
+    st.info(
+        "Faça upload da planilha para iniciar."
+    )
